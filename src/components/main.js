86742ced2d7e94971ba32e74c3699248a78c5643
fuser312 <sherltonGT40000>
2020-07-06 13:08:04 +0530

    Routing

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import LandingPage from "./landingPage";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Router} from "react-router-dom";
 import Projects from "./projects";
 import Resume from "./resume";
 import AboutMe from "./aboutMe";
@@ -9,13 +9,18 @@ import ContactMe from "./contactMe";
 
 const Main = () => {
     return (
+        <Router basename={'/profile'}>
   <Switch>
+
       <Route exact path ="/" component ={LandingPage}/>
-      <Route path ="/resume" component ={Resume}/>
-      <Route path ="/contactme" component ={ContactMe}/>
-      <Route exact path ="/aboutme" component ={AboutMe}/>
-      <Route exact path ="/projects" component ={Projects}/>
+      <Route path ="/resume/" component ={Resume}/>
+      <Route path ="/contactme/" component ={ContactMe}/>
+      <Route exact path ="/aboutme/" component ={AboutMe}/>
+      <Route exact path ="/projects/" component ={Projects}/>
+   
   </Switch>
+        </Router>
+
     );
 }
 
